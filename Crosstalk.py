df36fd29eda8ff35720d5193bea9d5431c312a66
charlies-world <charliew3169@gmail.com>
2021-04-18 16:10:49 -0400

    Added read_in_audio_files_method

--- Crosstalk.py
+++ Crosstalk.py
@@ -1,14 +1,26 @@
 from compoyse.wav.AudioFile import AudioFile
 from compoyse.wav.AudioClip import AudioClip
 from compoyse.wav.AudioPlayer import AudioPlayer
+import os
 
-def Crosstalk:
+class Crosstalk:
     def __init__(self):
+        self.audio_files = []
         return
     
     def set_parameters(self, parameters):
         self.parameters = parameters
-    return
+        return
 
     def perform(self):
+        self.read_in_audio_files()
+        return
+    
+    def read_in_audio_files(self):
+        file_directory = self.parameters.get_directory_for_files()
+        for file in os.scandir(file_directory):
+            new_audio_file = AudioFile()
+            new_audio_file.set_file_name(file.name)
+            new_audio_file.set_file_directory(file_directory)
+            self.audio_files.append(new_audio_file)
         return
--- Workspace.py
+++ Workspace.py
@@ -2,6 +2,7 @@ from Crosstalk import Crosstalk
 from Parameters import Parameters
 
 p = Parameters()
+p.set_directory_for_files("C:\\Users\\ewatts3\\Documents\\ImaginaryLandscape\\Audio\\Bass Communion II")
 #set parameters
 
 ct = Crosstalk()
